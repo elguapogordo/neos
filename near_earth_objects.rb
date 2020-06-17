9ebf844adfc7679edc75be9be306519e33bc2645
Jesse Gietzen <gietzenjesse@gmail.com>
2020-06-16 19:33:35 -0600

    pause for branch checkout

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -7,31 +7,39 @@ Figaro.load
 
 class NearEarthObjects
   def self.find_neos_by_date(date)
-    conn = Faraday.new(
-      url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
-    )
-    asteroids_list_data = conn.get('/neo/rest/v1/feed')
+    parsed_asteroids_data = asteroids_list_data(date)
+    {
+      asteroid_list: formatted_asteroid_data(parsed_asteroids_data),
+      biggest_asteroid: largest_asteroid_diameter(parsed_asteroids_data),
+      total_number_of_asteroids: parsed_asteroids_data.count
+    }
+  end
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-
-    largest_asteroid_diameter = parsed_asteroids_data.map do |asteroid|
-      asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
-    end.max { |a,b| a<=> b}
-
-    total_number_of_asteroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |asteroid|
+  def self.formatted_asteroid_data(parsed_asteroids_data)
+    parsed_asteroids_data.map do |asteroid|
+      binding.pry
       {
         name: asteroid[:name],
         diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
         miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
       }
     end
+  end
 
-    {
-      asteroid_list: formatted_asteroid_data,
-      biggest_asteroid: largest_asteroid_diameter,
-      total_number_of_asteroids: total_number_of_asteroids
-    }
+  def self.largest_asteroid_diameter(parsed_asteroids_data)
+    parsed_asteroids_data.map do |asteroid|
+      asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+    end.max { |a,b| a<=> b}
+  end
+
+  def self.asteroids_list_data(date)
+    response = conn.get('neo/rest/v1/feed') { |c| c.params[:start_date] = date }
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new('https://api.nasa.gov') do |f|
+      f.params[:api_key] = ENV['nasa_api_key']
+    end
   end
 end
